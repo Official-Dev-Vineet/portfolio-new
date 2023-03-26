5227062fe04b95074b4ffe0893beeebcdcd71618
vineet <viratsinghkaharwar8923@gmail.com>
2023-03-26 22:16:56 +0530

    NAVBAR  EDIT

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
-const nav=document.querySelector("header")
-window.addEventListener("scroll",e=>{
-    if(window.scrollY > 20){
+const nav = document.querySelector("header")
+window.addEventListener("scroll", e => {
+    if (window.scrollY > 20) {
         nav.classList.add("active")
     }
-    else{
+    else {
         nav.classList.remove("active")
     }
+})
+const hamburger = document.querySelector(".menu")
+const menu = document.querySelector("nav")
+hamburger.addEventListener("click", e => {
+    menu.classList.toggle("active");
 })
