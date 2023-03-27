68e93ec330125df1e689cc3f11d2f46a045a4c6c
vineet <viratsinghkaharwar8923@gmail.com>
2023-03-27 18:15:23 +0530

    go to top button added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const nav = document.querySelector("header")
+const myButton = document.getElementById("myBtn");
 window.addEventListener("scroll", e => {
     if (window.scrollY > 20) {
         nav.classList.add("active")
@@ -6,6 +7,15 @@ window.addEventListener("scroll", e => {
     else {
         nav.classList.remove("active")
     }
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
+})
+myButton.addEventListener('click', function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 })
 const hamburger = document.querySelector(".menu")
 const menu = document.querySelector("nav")
@@ -15,13 +25,26 @@ hamburger.addEventListener("click", e => {
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         entry.target.classList.toggle("animated", entry.isIntersecting)
-        if(entry.isIntersecting) return observer.unobserve(entry.target)
+        if (entry.isIntersecting) return observer.unobserve(entry.target)
     })
 }, {
     threshold: 1,
-    rootMargin:"100px"
+    rootMargin: "100px"
 })
 const animate = document.querySelectorAll(".animate")
 animate.forEach((element) => {
     observer.observe(element)
 })
+const textarea = document.querySelector("#textarea")
+textarea.addEventListener("scrollHeightChange", e => {
+    console.log(e.target.value)
+})
+
+const scrollHandler = () => {
+
+}
+const links = document.querySelectorAll("nav ul li")
+links.forEach((link) => {
+    link.addEventListener("click", scrollHandler)
+})
+
