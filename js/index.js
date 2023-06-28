84f5714a3c6c0ed8bf74039752f9dfbc4b58ed62
vineet <viratsinghkaharwar8923@gmail.com>
2023-06-28 07:50:43 +0530

    add new project

--- js/index.js
+++ js/index.js
@@ -1,101 +1,121 @@
 "use strict";
-const nav = document.querySelector("header")
+const nav = document.querySelector("header");
 const myButton = document.getElementById("myBtn");
-window.addEventListener("scroll", e => {
-    if (window.scrollY > 20) {
-        nav.classList.add("active")
-    }
-    else {
-        nav.classList.remove("active")
-    }
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        myButton.style.display = "block";
-    } else {
-        myButton.style.display = "none";
-    }
-})
-myButton.addEventListener('click', function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-})
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY > 20) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+});
+myButton.addEventListener("click", function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
 
-// navbar handler 
-const hamburger = document.querySelector(".menu")
-const menu = document.querySelector("nav")
-hamburger.addEventListener("click", e => {
-    menu.classList.toggle("active");
-})
+// navbar handler
+const hamburger = document.querySelector(".menu");
+const menu = document.querySelector("nav");
+hamburger.addEventListener("click", (e) => {
+  menu.classList.toggle("active");
+});
 
-// intersection observer scripting 
+// intersection observer scripting
 
-const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver(
+  (entries) => {
     entries.forEach((entry) => {
-        entry.target.classList.toggle("animated", entry.isIntersecting)
-        if (entry.isIntersecting) return observer.unobserve(entry.target)
-    })
-}, {
+      entry.target.classList.toggle("animated", entry.isIntersecting);
+      if (entry.isIntersecting) return observer.unobserve(entry.target);
+    });
+  },
+  {
     threshold: 1,
-    rootMargin: "100px"
-})
-const animate = document.querySelectorAll(".animate")
+    rootMargin: "100px",
+  }
+);
+const animate = document.querySelectorAll(".animate");
 animate.forEach((element) => {
-    observer.observe(element)
-})
-const textarea = document.querySelector("#textarea")
-textarea?.addEventListener("keyup", e => {
-    textarea.style.height = e.currentTarget.scrollHeight + "px";
-})
+  observer.observe(element);
+});
+const textarea = document.querySelector("#textarea");
+textarea?.addEventListener("keyup", (e) => {
+  textarea.style.height = e.currentTarget.scrollHeight + "px";
+});
 
 // hide navigation when click on nav links
-const links = document.querySelectorAll("nav ul li")
+const links = document.querySelectorAll("nav ul li");
 
 links.forEach((link) => {
-    link.addEventListener('click', e => {
-        menu?.classList?.remove("active")
-    })
-})
+  link.addEventListener("click", (e) => {
+    menu?.classList?.remove("active");
+  });
+});
 
 // apply filter
-const tags = document.querySelectorAll(".tags span")
-const filterName = document.querySelector(".filter input")
-filterName.onfocus=()=>{
-    filterName.value=""
-}
-const filterBtn = document.querySelector(".filter button")
-const projects = document.querySelectorAll(".projects .project")
-filterBtn.addEventListener('click', e => {
-    let filterValue = filterName.value.trim()
-    projects.forEach((project) => {
-        let tags = project.querySelectorAll(".tags span")
-        let tagsData = []
-        tags.forEach((tag) => {
-            tagsData.push(tag.textContent)
-        })
-        if (tagsData.includes(filterValue)) {
-            project.style.display = "block"
-        }
-        else if (filterValue == "all") {
-            project.style.display = "block"
-        }
-        else if (filterValue == "") {
-        } else {
-            project.style.display = "none"
-        }
-    })
-})
+const tags = document.querySelectorAll(".tags span");
+const filterName = document.querySelector(".filter input");
+filterName.onfocus = () => {
+  filterName.value = "";
+};
+const filterBtn = document.querySelector(".filter button");
+const projects = document.querySelectorAll(".projects .project");
+filterBtn.addEventListener("click", (e) => {
+  let filterValue = filterName.value.trim();
+  projects.forEach((project) => {
+    let tags = project.querySelectorAll(".tags span");
+    let tagsData = [];
+    tags.forEach((tag) => {
+      tagsData.push(tag.textContent);
+    });
+    if (tagsData.includes(filterValue)) {
+      project.style.display = "block";
+    } else if (filterValue == "all") {
+      project.style.display = "block";
+    } else if (filterValue == "") {
+    } else {
+      project.style.display = "none";
+    }
+  });
+});
 
-let filteredTags = []
+let filteredTags = [];
 tags.forEach((tag) => {
-    filteredTags.push(tag.textContent.trim())
-    filteredTags.sort()
-})
-const newFilterTags = new Set(filteredTags)
+  filteredTags.push(tag.textContent.trim());
+  filteredTags.sort();
+});
+const newFilterTags = new Set(filteredTags);
 // filteredTags=Array.from(newFilterTags)
-filteredTags = [...newFilterTags]
-const datalist = document.querySelector("datalist")
+filteredTags = [...newFilterTags];
+const datalist = document.querySelector("datalist");
 filteredTags.map((tag) => {
-    let option = document.createElement("option")
-    option.value = tag
-    option.textContent = tag
-    datalist.appendChild(option)
+  let option = document.createElement("option");
+  option.value = tag;
+  option.textContent = tag;
+  datalist.appendChild(option);
+});
+
+// form validator
+const submitBtn = document.querySelector("#submit");
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const formData = document.querySelector("form");
+  const data = new FormData(formData);
+  const name = data.get("clientName").trim();
+  const email = data.get("clientEmail").trim();
+  const subject = data.get("subject").trim();
+  const projectDetails = data.get("projectDetails").trim();
+  window.location.href = `https://wa.me/+7983920962&text=my name is ${name}. \n and my email id : ${email} \n i msg to you regarding ${subject} and i have an project idea : ${projectDetails}`;
+});
+
+
+// target blank generator
+const targetedLink=document.querySelectorAll(".button-group a") 
+targetedLink.forEach((link)=>{
+  link.setAttribute("target","_blank")
 })
