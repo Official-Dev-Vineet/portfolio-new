0b3290b6f79fb9fa67224f9fda4f51a9f60bbc54
vineet <viratsinghkaharwar8923@gmail.com>
2023-07-22 00:14:16 +0530

    added keyup search functionality

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 "use strict";
 const nav = document.querySelector("header");
 const myButton = document.getElementById("myBtn");
+const filterBtn = document.querySelector(".filter button");
+const projects = document.querySelectorAll(".projects .project");
+const hamburger = document.querySelector(".menu");
+const menu = document.querySelector("nav");
+const animate = document.querySelectorAll(".animate");
+const textarea = document.querySelector("textarea");
+const searchResult = document.querySelector("#searchResult");
+
 window.addEventListener("scroll", (e) => {
   if (window.scrollY > 20) {
     nav.classList.add("active");
@@ -19,8 +27,6 @@ myButton.addEventListener("click", () => {
 });
 
 // navbar handler
-const hamburger = document.querySelector(".menu");
-const menu = document.querySelector("nav");
 hamburger.addEventListener("click", (e) => {
   menu.classList.toggle("active");
 });
@@ -39,13 +45,8 @@ const observer = new IntersectionObserver(
     rootMargin: "100px",
   }
 );
-const animate = document.querySelectorAll(".animate");
 animate.forEach((element) => {
   observer.observe(element);
-});
-const textarea = document.querySelector("#textarea");
-textarea?.addEventListener("keyup", (e) => {
-  textarea.style.height = e.currentTarget.scrollHeight + "px";
 });
 
 // hide navigation when click on nav links
@@ -57,29 +58,48 @@ links.forEach((link) => {
   });
 });
 
+function debounce(func, wait) {
+  let timer = null;
+  clearTimeout(timer);
+  return (timer = setTimeout(() => {
+    func();
+  }, wait));
+}
 // apply filter
 const tags = document.querySelectorAll(".tags span");
 const filterName = document.querySelector(".filter input");
+filterName.addEventListener("keyup", () => debounce(filterProject, 1000));
 filterName.onfocus = () => {
   filterName.value = "";
 };
-const filterBtn = document.querySelector(".filter button");
-const projects = document.querySelectorAll(".projects .project");
-filterBtn.addEventListener("click", (e) => {
-  let filterValue = filterName.value.trim();
+
+// filter function call
+filterBtn.addEventListener("click", filterProject());
+
+// filter function
+function filterProject() {
+  let filterValue = filterName.value.trim().toLowerCase();
   projects.forEach((project) => {
     let tags = project.querySelectorAll(".tags span");
     let tagsData = [];
     tags.forEach((tag) => {
       tagsData.push(tag.textContent);
     });
-    if (tagsData.includes(filterValue)) return project.style.display = "block";
-    if ( filterValue === "all") return project.style.display = "block";
-    if (filterValue === "") return 
-    project.style.display = "none";
+    const tagString = tagsData.join(",").toLowerCase();
+    if (tagString.includes(filterValue)) {
+      searchResult.textContent = "Here is your project";
+      project.style.display = "block";
+    } else {
+      searchResult.textContent = "No Match Related to Query";
+      project.style.display = "none";
+    }
+    if (filterValue === "all") {
+      searchResult.textContent = "All projects has been shown";
+      project.style.display = "block";
+    }
   });
-});
-
+}
+// dynamic generate new and old tags and append it to input data list
 let filteredTags = [];
 tags.forEach((tag) => {
   filteredTags.push(tag.textContent.trim());
@@ -112,6 +132,8 @@ submitBtn.addEventListener("click", (e) => {
   projectDetails.length > 0
     ? submitData()
     : alert("Please fill correct details !");
+
+  // submit data to a whatsApp number
   function submitData() {
     const date = new Date();
     window.location.href = `https://wa.me/917983920962?text=Name :${name},%0AEmail :${email}%0A Subject : ${subject}%0A Project Idea : ${projectDetails}%0A Date : ${date} %0A this is send from Vineet Singh Portfolio and below is the link : %0A ${window.location.href}`;
@@ -125,7 +147,6 @@ targetedLink.forEach((link) => {
 });
 
 // typing js
-
 var typed = new Typed(".typing", {
   strings: ["Developer", "Freelancer", "Debugger"],
   typeSpeed: 200,
@@ -134,6 +155,7 @@ var typed = new Typed(".typing", {
 });
 
 //theme setup
+// primary color generator
 let color = document.querySelectorAll("#theme ul li span").forEach((e) => {
   e.style.backgroundColor = `${e.getAttribute("data-color")}`;
   e.onclick = () => {
@@ -146,6 +168,7 @@ let color = document.querySelectorAll("#theme ul li span").forEach((e) => {
 document.querySelector(".theme").onclick = () => {
   document.querySelector("#theme").classList.toggle("active");
 };
+// secondary color generator
 document.querySelector("#color").oninput = () => {
   let color = document.querySelector("#color").value;
   document.querySelector(":root").style.setProperty("--secondary", color);
