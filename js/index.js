46395e0bb2b8f095c2d4b3e0d7f5d3e14f29367a
vineet <viratsinghkaharwar8923@gmail.com>
2023-07-28 21:19:54 +0530

    remove ocilator js animation and added self designed bg

--- js/index.js
+++ js/index.js
@@ -211,3 +211,37 @@ const msgShower = () => {
   }, 5000);
 };
 msgShower();
+
+// background animation generator
+
+function backgroundGenerator() {
+  const color = () => {
+    var color = `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
+    return color;
+  };
+  let bg = document.querySelector(".bg");
+  for (let i = 1; i < window.innerWidth; i++, i += 100) {
+    for (let j = 1; j < window.innerHeight; j++, j += 100) {
+      let span = document.createElement("span");
+      span.style.background = color();
+      span.style.borderRadius =
+        Math.floor(Math.random() * 0xff).toString(16) + "%";
+      span.style.scale = Math.random();
+      bg.appendChild(span);
+    }
+  }
+  let span = document.querySelectorAll(".bg span");
+  setInterval(() => {
+    span.forEach((element, index) => {
+      element.style.background = color();
+      element.style.borderRadius =
+        Math.floor(Math.random() * 0xff).toString(16) + "%";
+      element.style.scale = Math.random() * 2;
+      element.style.opacity = Math.random();
+
+      element.style.rotate = Math.floor(Math.random() * 360).toString() + "deg";
+    });
+  }, 3000);
+}
+document.body.addEventListener("orientationchange", backgroundGenerator());
+window.addEventListener("resize", backgroundGenerator());
