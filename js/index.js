a5bf92619e1ceaf8ad8ca9f8644970661666cf08
vineet <viratsinghkaharwar8923@gmail.com>
2023-07-28 22:09:49 +0530

    fixed memory consuming issue

--- js/index.js
+++ js/index.js
@@ -213,19 +213,18 @@ const msgShower = () => {
 msgShower();
 
 // background animation generator
-
+let bg = document.querySelector(".bg");
+let timerBg = null;
 function backgroundGenerator() {
+  bg.innerHTML = "";
   const color = () => {
     var color = `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
     return color;
   };
-  let bg = document.querySelector(".bg");
   for (let i = 1; i < window.innerWidth; i++, i += 100) {
     for (let j = 1; j < window.innerHeight; j++, j += 100) {
       let span = document.createElement("span");
       span.style.background = color();
-      span.style.borderRadius =
-        Math.floor(Math.random() * 0xff).toString(16) + "%";
       span.style.scale = Math.random();
       bg.appendChild(span);
     }
@@ -234,14 +233,15 @@ function backgroundGenerator() {
   setInterval(() => {
     span.forEach((element, index) => {
       element.style.background = color();
-      element.style.borderRadius =
-        Math.floor(Math.random() * 0xff).toString(16) + "%";
-      element.style.scale = Math.random() * 2;
-      element.style.opacity = Math.random();
-
+      element.style.borderRadius = Math.floor(Math.random() * 100) + "%";
       element.style.rotate = Math.floor(Math.random() * 360).toString() + "deg";
     });
   }, 3000);
 }
-document.body.addEventListener("orientationchange", backgroundGenerator());
-window.addEventListener("resize", backgroundGenerator());
+backgroundGenerator();
+window.onresize = () => {
+  clearInterval(timerBg);
+  clearInterval(timer);
+  backgroundGenerator();
+  msgShower();
+};
