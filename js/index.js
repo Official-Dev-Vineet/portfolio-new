7c01bae4ddc11381919d677230c24e892e876038
official-dev-vineet <tripetrip01@gmail.com>
2024-01-07 21:50:08 +0530

    update for real time data

--- js/index.js
+++ js/index.js
@@ -264,7 +264,7 @@ window.addEventListener("load", () => {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      fetch("http://localhost:5000/api/ipLogger", {
+      fetch("https://breakable-fish-crown.cyclic.app/api/ipLogger", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
